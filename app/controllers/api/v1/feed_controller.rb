365654dfa1beac5fa11d64b23635f28c115b2ea8
Or Rubin <or.rubin@hotmail.com>
2017-10-24 10:01:27 +0300

    Add post and user and migrations

--- app/controllers/api/v1/feed_controller.rb
+++ app/controllers/api/v1/feed_controller.rb
@@ -5,11 +5,19 @@ class Api::V1::FeedController < Api::V1::ApplicationController
   end
 
   def get_feed
+    render json: 'code: 400' if check_params(params)
+
+    Post.get_posts_for_user(params[:user_id], params[:lat], params[:lng])
+
     first_post = { name: 'first', lat: 32.000, lng: 34.000, img: 'http://www.oround.polak/image.gif'}
     second_post = { name: 'second', lat: 32.000, lng: 34.000, img: 'http://www.oround.polak/image.gif'}
     third_post = { name: 'third', lat: 32.000, lng: 34.000, img: 'http://www.oround.polak/image.gif'}
     feed = [ first_post, second_post, third_post]
     render json: feed
+  end
+
+  def check_params(params)
+    return false if params.empty?
   end
 
 end
