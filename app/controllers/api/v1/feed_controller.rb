dec11aa465fb7996534235f9550bd2eda5348fbb
Or Rubin <or.rubin@hotmail.com>
2017-10-28 16:43:26 +0300

    Fix for webserver

--- app/controllers/api/v1/feed_controller.rb
+++ app/controllers/api/v1/feed_controller.rb
@@ -5,14 +5,14 @@ class Api::V1::FeedController < Api::V1::ApplicationController
   end
 
   def get_feed
-    render json: 'code: 400' if check_params(params)
+    #render json: 'code: 400' if check_params(params)
 
-    posts = Post.get_posts_for_user(params[:user_id], params[:lat], params[:lng])
+    #posts = Post.get_posts_for_user(params[:user_id], params[:lat], params[:lng])
 
     first_post = { name: 'first', lat: 32.000, lng: 34.000, img: 'http://www.oround.polak/image.gif'}
     second_post = { name: 'second', lat: 32.000, lng: 34.000, img: 'http://www.oround.polak/image.gif'}
     third_post = { name: 'third', lat: 32.000, lng: 34.000, img: 'http://www.oround.polak/image.gif'}
-    feed = [ first_post, second_post, third_post]
+    feed = [first_post, second_post, third_post]
     render json: feed
   end
 
