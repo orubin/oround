4388975decf7b832955e45468bd83090a7706fe1
Or Rubin <or.rubin@hotmail.com>
2017-11-06 08:19:09 +0200

    Fix images

--- app/controllers/api/v1/feed_controller.rb
+++ app/controllers/api/v1/feed_controller.rb
@@ -9,9 +9,9 @@ class Api::V1::FeedController < Api::V1::ApplicationController
 
     #posts = Post.get_posts_for_user(params[:user_id], params[:lat], params[:lng])
 
-    first_post = { name: 'first', lat: 32.000, lng: 34.000, img: 'http://www.oround.polak/image.gif'}
-    second_post = { name: 'second', lat: 32.000, lng: 34.000, img: 'http://www.oround.polak/image.gif'}
-    third_post = { name: 'third', lat: 32.000, lng: 34.000, img: 'http://www.oround.polak/image.gif'}
+    first_post = { name: 'first', lat: 32.000, lng: 34.000, img: 'http://www.ozarlington.com/wp-content/uploads/2017/04/bar-buffet.jpg'}
+    second_post = { name: 'second', lat: 32.000, lng: 34.000, img: 'https://media-cdn.tripadvisor.com/media/photo-s/0a/7b/7b/c6/restaurant-view.jpg'}
+    third_post = { name: 'third', lat: 32.000, lng: 34.000, img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Tom%27s_Restaurant%2C_NYC.jpg/1200px-Tom%27s_Restaurant%2C_NYC.jpg'}
     feed = [first_post, second_post, third_post]
     render json: { code: 200, data: feed }
   end
